ce61d9f738570ec0336d0f52ceb35bb3dbc62094
Connie Ho <connie3@ualberta.ca>
2021-02-04 19:07:10 +0000

    finishing index

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+
+  if (!array.length) return sum;
+
+  for (const elem of array) {
+    if (Array.isArray(elem)) {
+      sum += sumItems(elem);
+    } else {
+      sum += elem;
+    }
+  }s
+  return sum;
 }
+
+// console.log(sumItems([1, 2, 3, 4, 5]));
+// console.log(sumItems([1, [2, 3]]))
+// console.log(sumItems([[ 1, 2, [[3], 4]], 5, []]))
 
 module.exports = sumItems;
